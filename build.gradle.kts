a58adc96002ece6f78e07b96f3d4783acc4ee5f3
Joe van der Zwet <me@joezwet.me>
2020-10-10 21:00:45 +1300

    feat: initial java project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+group = "com.iosoftworks.st"
+version = "0.1.0-alpha+build.000"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit", "junit", "4.12")
+}
