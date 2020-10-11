38f5fa652dfb63ba038e334d23fd7beee432c38f
James <Jameshessell@gmail.com>
2020-10-12 12:47:38 +1300

    feat: RollerFirstTest

--- src/test/java/com/iosoftworks/st/assignmenttwo/tests/RollerTest.java
+++ src/test/java/com/iosoftworks/st/assignmenttwo/tests/RollerTest.java
@@ -3,17 +3,12 @@ package com.iosoftworks.st.assignmenttwo.tests;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class RollerTest {
     @Test
-    public void rollMins_expectZero_shouldFail(){
-       Assert.assertEquals(Roller.roll(), 0);
-    }
-    @Test
-    public void rollMins_expectNegativeOne_shouldFail(){
-        Assert.assertEquals(Roller.roll(), -1);
-    }
-    @Test
-    public void rollMins_expectNegativeOne_shouldFail(){
-        Roller.roll(Integer.MIN_VALUE);
-    }
+    public void rollMins_shouldFail(){
+        int high = 0;
+        assertTrue("Error, random is too low",  high  <= Roller.roll());    }
+
 }
