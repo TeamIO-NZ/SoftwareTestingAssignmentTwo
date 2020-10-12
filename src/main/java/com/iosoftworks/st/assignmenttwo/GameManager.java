f3d5e64fbaf8d1674112d238348aaf93cb6df0f2
Joe van der Zwet <me@joezwet.me>
2020-10-12 13:05:15 +1300

    feat: make `players` final.

--- src/main/java/com/iosoftworks/st/assignmenttwo/GameManager.java
+++ src/main/java/com/iosoftworks/st/assignmenttwo/GameManager.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameManager {
-    private List<Player> players = new ArrayList<>();
+    private final List<Player> players = new ArrayList<>();
     private static GameManager instance;
 
     private GameManager() {
