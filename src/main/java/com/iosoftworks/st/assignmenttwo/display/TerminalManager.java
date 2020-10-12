46e6506beaf07c3763b09acf2a2588b7e77bc6ce
Joe van der Zwet <me@joezwet.me>
2020-10-12 15:55:13 +1300

    feat: terminal resizing

--- src/main/java/com/iosoftworks/st/assignmenttwo/display/TerminalManager.java
+++ src/main/java/com/iosoftworks/st/assignmenttwo/display/TerminalManager.java
@@ -2,17 +2,13 @@ package com.iosoftworks.st.assignmenttwo.display;
 
 import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.graphics.TextImage;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.iosoftworks.st.assignmenttwo.GameManager;
 
-import javax.swing.*;
 import java.io.IOException;
-import java.util.Random;
 
 public class TerminalManager {
 
@@ -30,54 +26,30 @@ public class TerminalManager {
             term.enterPrivateMode();
             term.setCursorVisible(false);
             term.clearScreen();
+            drawOutline(term);
+            drawPlayers(term);
+            boolean playerTurn = false;
+            drawTurnIndicator(term, playerTurn);
 
-            TextGraphics graphics = term.newTextGraphics();
-            TerminalPosition pos = term.getCursorPosition();
-            TerminalSize size = term.getTerminalSize();
-            graphics.drawLine(
-                    pos.withRelativeColumn(1),
-                    pos.withRelativeColumn(size.getColumns() - 2),
-                    Symbols.DOUBLE_LINE_HORIZONTAL);
-            graphics.drawLine(
-                    pos.withRelative(1, size.getRows() - 1),
-                    pos.withRelative(size.getColumns() - 2, size.getRows() - 1),
-                    Symbols.DOUBLE_LINE_HORIZONTAL);
-            graphics.drawLine(
-                    pos.withRelative(0, 1),
-                    pos.withRelative(0, size.getRows() - 2),
-                    Symbols.DOUBLE_LINE_VERTICAL);
-            graphics.drawLine(
-                    pos.withRelative(size.getColumns() - 1, 1),
-                    pos.withRelative(size.getColumns() - 1, size.getRows() - 2),
-                    Symbols.DOUBLE_LINE_VERTICAL);
-            graphics.putString(
-                    0,
-                    0,
-                    String.valueOf(Symbols.DOUBLE_LINE_TOP_LEFT_CORNER));
-            graphics.putString(
-                    size.getColumns() - 1,
-                    0,
-                    String.valueOf(Symbols.DOUBLE_LINE_TOP_RIGHT_CORNER));
-            graphics.putString(
-                    0,
-                    size.getRows() - 1,
-                    String.valueOf(Symbols.DOUBLE_LINE_BOTTOM_LEFT_CORNER));
-            graphics.putString(
-                    size.getColumns() - 1,
-                    size.getRows() - 1,
-                    String.valueOf(Symbols.DOUBLE_LINE_BOTTOM_RIGHT_CORNER));
-            graphics.putString(
-                    ((size.getColumns() / 2) - ("Dice Black Jack - Press ESC to exit".length() / 2)) - 1,
-                    1,
-                    "Dice Black Jack - Press ESC to exit",
-                    SGR.BOLD
-            );
-            term.flush();
-
-
+            boolean finalPlayerTurn = playerTurn;
+            term.addResizeListener((terminal, newSize) -> {
+                try {
+                    terminal.clearScreen();
+                    drawOutline(terminal);
+                    drawPlayers(terminal);
+                    drawTurnIndicator(terminal, finalPlayerTurn);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
 
             KeyStroke stroke = term.readInput();
             while (stroke.getKeyType() != KeyType.Escape && stroke.getKeyType() != KeyType.EOF) {
+                if (stroke.getCharacter() == ' ') {
+                    playerTurn = !playerTurn;
+                    drawTurnIndicator(term, playerTurn);
+                }
+
                 stroke = term.readInput();
             }
 
@@ -95,5 +67,72 @@ public class TerminalManager {
                 }
             }
         }
+    }
+
+    private void drawOutline(Terminal term) throws IOException {
+        TextGraphics graphics = term.newTextGraphics();
+        TerminalPosition pos = term.getCursorPosition();
+        TerminalSize size = term.getTerminalSize();
+        graphics.drawLine(
+                pos.withRelativeColumn(1),
+                pos.withRelativeColumn(size.getColumns() - 2),
+                Symbols.DOUBLE_LINE_HORIZONTAL);
+        graphics.drawLine(
+                pos.withRelative(1, size.getRows() - 1),
+                pos.withRelative(size.getColumns() - 2, size.getRows() - 1),
+                Symbols.DOUBLE_LINE_HORIZONTAL);
+        graphics.drawLine(
+                pos.withRelative(0, 1),
+                pos.withRelative(0, size.getRows() - 2),
+                Symbols.DOUBLE_LINE_VERTICAL);
+        graphics.drawLine(
+                pos.withRelative(size.getColumns() - 1, 1),
+                pos.withRelative(size.getColumns() - 1, size.getRows() - 2),
+                Symbols.DOUBLE_LINE_VERTICAL);
+        graphics.putString(
+                0,
+                0,
+                String.valueOf(Symbols.DOUBLE_LINE_TOP_LEFT_CORNER));
+        graphics.putString(
+                size.getColumns() - 1,
+                0,
+                String.valueOf(Symbols.DOUBLE_LINE_TOP_RIGHT_CORNER));
+        graphics.putString(
+                0,
+                size.getRows() - 1,
+                String.valueOf(Symbols.DOUBLE_LINE_BOTTOM_LEFT_CORNER));
+        graphics.putString(
+                size.getColumns() - 1,
+                size.getRows() - 1,
+                String.valueOf(Symbols.DOUBLE_LINE_BOTTOM_RIGHT_CORNER));
+        String title = "Dice Black Jack - Press ESC to exit";
+        graphics.putString(((size.getColumns() / 2) - (title.length() / 2)) - 3, 0, "\u2561 ");
+        graphics.putString(((size.getColumns() / 2) + (title.length() / 2)) - 1, 0, " \u255e");
+        graphics.putString(
+                ((size.getColumns() / 2) - (title.length() / 2)) - 1,
+                0,
+                title,
+                SGR.BOLD);
+
+        term.flush();
+    }
+
+    private void drawPlayers(Terminal term) throws IOException {
+        TextGraphics graphics = term.newTextGraphics();
+        TerminalSize size = term.getTerminalSize();
+        graphics.putString(2, 5, "You", SGR.BOLD);
+        graphics.putString(size.getColumns() - 4, 5, "AI", SGR.BOLD);
+
+        term.flush();
+    }
+
+    private void drawTurnIndicator(Terminal term, boolean playerTurn) throws IOException {
+        TerminalSize size = term.getTerminalSize();
+        TextGraphics graphics = term.newTextGraphics();
+
+        graphics.putString(6, 5, playerTurn ? String.valueOf(Symbols.ARROW_LEFT) : " ", SGR.BOLD);
+        graphics.putString(size.getColumns() - 7, 5, playerTurn ? " " : String.valueOf(Symbols.ARROW_RIGHT), SGR.BOLD);
+
+        term.flush();
     }
 }
