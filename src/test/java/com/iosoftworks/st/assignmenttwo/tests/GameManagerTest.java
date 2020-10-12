3a35f2e93fe7c9a3c9c9d9b9d8aa65edb768377c
Joe van der Zwet <me@joezwet.me>
2020-10-12 13:00:47 +1300

    fix: fixed @before in GameManagerTest

--- src/test/java/com/iosoftworks/st/assignmenttwo/tests/GameManagerTest.java
+++ src/test/java/com/iosoftworks/st/assignmenttwo/tests/GameManagerTest.java
@@ -1,23 +1,22 @@
 package com.iosoftworks.st.assignmenttwo.tests;
 
 import com.iosoftworks.st.assignmenttwo.GameManager;
-import com.iosoftworks.st.assignmenttwo.models.player.Player;
 import com.iosoftworks.st.assignmenttwo.models.player.ai.AIPlayer;
 import com.iosoftworks.st.assignmenttwo.models.player.human.HumanPlayer;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GameManagerTest {
 
-    @Before
-    public void init(){
+    @BeforeClass
+    public static void init(){
         GameManager.getInstance().getPlayers().add(new HumanPlayer());
         GameManager.getInstance().getPlayers().add(new AIPlayer());
     }
     @Test
     public void CheckPlayerCount_ExpectingTwo(){
-        Assert.assertEquals(GameManager.getInstance().getPlayers().size(), 2);
+        Assert.assertEquals(2, GameManager.getInstance().getPlayers().size());
     }
     @Test
     public void CheckPlayersExist_ExpectingTrue(){
