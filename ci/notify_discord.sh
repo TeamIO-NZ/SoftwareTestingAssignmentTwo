696a5405c9c2160972f3708c6fdcfcd43116c7cb
Joe van der Zwet <me@joezwet.me>
2020-10-26 16:01:25 +1300

    ci: add discord notification to ci

---
+++ ci/notify_discord.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# This script is used to notify discord of post-build status
+
+# ./ci/notify_discord.sh "webhook url" "Project Name" status
+# success abort error failure
+
+
+if [ "$3" == "success" ]
+then
+    color="2084461"
+    message="build on $2 was successful"
+elif [ "$3" == "abort" ]
+then
+    color="9131818"
+    message="build on $2 was aborted"
+elif [ "$3" == "error" ]
+then
+    color="16098851"
+    message="build on $2 errored"
+elif [ "$3" == "failure" ]
+then
+    color="15551285"
+    message="build on $2 failed"
+fi
+
+timestamp=$(date --utc -I'seconds')
+timestamp="${timestamp%"UTC"}Z"
+json="{\"embeds\":[{\"title\":\"Build on $2\",\"description\":\"$message\",\"url\":\"http://office.iosoftworks.net:8080/teams/main/pipelines/$2\",\"timestamp\":\"$timestamp\",\"color\":\"$color\"}]}"
+
+curl -X POST --data "$json" --header "Content-Type:application/json" "$1"
